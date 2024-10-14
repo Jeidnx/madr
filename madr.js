a4e35f9a7a44e2b7850e2ad0e8613d315a22643a
Jeidnx <jeidnx@domainhier.de>
2024-10-14 20:34:50 +0200

    chore: refactor code

--- madr.js
+++ madr.js
@@ -1,60 +1,64 @@
-const main = document.querySelectorAll('[role="main"]');
-const links = [];
-
-for (let i = 0; i < main.length; i++) {
-	const tags = main[i].getElementsByTagName('a');
-	for (let j = 0; j < tags.length; j++) links.push(tags[j]);
+async function openAsBlob(response) {
+	const blob = await response.blob();
+	const url = window.URL.createObjectURL(blob);
+	window.open(url);
+	window.URL.revokeObjectURL(url);
 }
+
+function load(a, link) {
+	document.body.style.cursor = 'wait';
+	a.style.cursor = 'wait';
+	fetch(link).then(openAsBlob).then(() => {
+		document.body.style.cursor = 'default'
+		a.style.cursor = 'pointer';
+	});
+	return false;
+}
+
+function loadAnnotator(a, link) {
+	document.body.style.cursor = 'wait';
+	a.style.cursor = 'wait';
+	fetch(link)
+		.then((res) => { return res.text(); })
+		.then((resText) => {
+			const dom = Document.parseHTMLUnsafe(resText);
+			const link = dom.getElementById("myprinturl").innerHTML;
+
+			return fetch(link);
+		}).then(openAsBlob).then(() => {
+			document.body.style.cursor = 'default'
+			a.style.cursor = 'pointer';
+		});
+	return false;
+}
+
+const script = document.createElement("script");
+
+// Inject functions into page DOM
+script.innerHTML = `
+	${openAsBlob.toString()}
+	${load.toString()}
+	${loadAnnotator.toString()}
+`;
+document.head.appendChild(script);
+
 
 const match = /(?:\/pluginfile.php\/[0-9]+\/mod_(?:folder\/content)|(?:assign\/introattachment)|(?:forum\/attachment)|(?:assignsubmission_file\/submission_files))|(?:\/mod\/resource\/view.php)/;
 
 const annotatorMatch = /(?:\/mod\/pdfannotator\/view.php)/;
 
-for (const elem of links) {
+const main = document.querySelectorAll('[role="main"]')[0];
+const anchors = main?.getElementsByTagName('a') ?? [];
+
+for (const elem of anchors) {
 	const link = elem.href;
-	if (match.test(elem.href)) {
-		elem.style.cursor = 'pointer';
-		elem.setAttribute('onclick', `
-			document.body.style.cursor = 'wait';
-			const a = this;
-			a.style.cursor = 'wait';
-			fetch('${link}').then((res) => {
-				return res.blob();
-			}).then((blob) => {
-				const url = window.URL.createObjectURL(blob);
-				document.body.style.cursor = 'default'
-				a.style.cursor = 'pointer';
-				window.open(url);
-				window.URL.revokeObjectURL(url);
-			});
-			return false;
-		`);
+
+	if (match.test(link)) {
+		elem.setAttribute('onclick', `return load(this, '${link}');`);
 		continue;
 	};
 
-	if (annotatorMatch.test(elem.href)) {
-		elem.style.cursor = 'pointer';
-		elem.setAttribute('onclick', `
-			document.body.style.cursor = 'wait';
-			const a = this;
-			a.style.cursor = 'wait';
-			fetch('${link}')
-				.then((res) => {return res.text();})
-				.then((resText) => {
-					const dom = Document.parseHTMLUnsafe(resText);
-					const link = dom.getElementById("myprinturl").innerHTML;
-					return fetch(link);
-				}).then((res) => {
-					return res.blob();
-				})
-				.then((blob) => {
-					const url = window.URL.createObjectURL(blob);
-					document.body.style.cursor = 'default'
-					a.style.cursor = 'pointer';
-					window.open(url);
-					window.URL.revokeObjectURL(url);
-				});
-			return false;
-			`)
+	if (annotatorMatch.test(link)) {
+		elem.setAttribute('onclick', `return loadAnnotator(this, '${link}');`)
 	}
 };
