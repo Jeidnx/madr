b360c0830d16665801aad09042fdca403e3c081e
Jeidnx <jeidnx@domainhier.de>
2024-10-14 17:08:07 +0200

    feat: add pdf annotator

--- madr.js
+++ madr.js
@@ -8,9 +8,11 @@ for (let i = 0; i < main.length; i++) {
 
 const match = /(?:\/pluginfile.php\/[0-9]+\/mod_(?:folder\/content)|(?:assign\/introattachment)|(?:forum\/attachment)|(?:assignsubmission_file\/submission_files))|(?:\/mod\/resource\/view.php)/;
 
-for (const elem of links)
+const annotatorMatch = /(?:\/mod\/pdfannotator\/view.php)/;
+
+for (const elem of links) {
+	const link = elem.href;
 	if (match.test(elem.href)) {
-		const link = elem.href;
 		elem.style.cursor = 'pointer';
 		elem.setAttribute('onclick', `
 			document.body.style.cursor = 'wait';
@@ -26,5 +28,33 @@ for (const elem of links)
 				window.URL.revokeObjectURL(url);
 			});
 			return false;
-		`)
+		`);
+		continue;
+	};
+
+	if (annotatorMatch.test(elem.href)) {
+		elem.style.cursor = 'pointer';
+		elem.setAttribute('onclick', `
+			document.body.style.cursor = 'wait';
+			const a = this;
+			a.style.cursor = 'wait';
+			fetch('${link}')
+				.then((res) => {return res.text();})
+				.then((resText) => {
+					const dom = Document.parseHTMLUnsafe(resText);
+					const link = dom.getElementById("myprinturl").innerHTML;
+					return fetch(link);
+				}).then((res) => {
+					return res.blob();
+				})
+				.then((blob) => {
+					const url = window.URL.createObjectURL(blob);
+					document.body.style.cursor = 'default'
+					a.style.cursor = 'pointer';
+					window.open(url);
+					window.URL.revokeObjectURL(url);
+				});
+			return false;
+			`)
 	}
+};
